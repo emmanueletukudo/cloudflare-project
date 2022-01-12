b7343a70d605b2e4b7a59f99c6c7680646d3c8dd
Emmanuel Etukudo <emzzyyoung@gmail.com>
2022-01-12 12:55:37 +0100

    update on server function

--- .functions/greetings.js
+++ .functions/greetings.js
@@ -1,13 +1,11 @@
 //GET Request to
+// GET requests to /filename would return "Hello, world!"
 export const onRequestGet = () => {
-  return new Response("Hellow World");
+  return new Response("Hello, world!")
 }
 
+// POST requests to /filename with a JSON-encoded body would return "Hello, <name>!"
 export const onRequestPost = async ({ request }) => {
-  const { name } = await request.json();
-
-  return new Response(`Hello ${name}`);
+  const { name } = await request.json()
+  return new Response(`Hello, ${name}!`)
 }
-
-
-
